63b2c109cdabd23ca78736131db56e74cd562492
Franck Kamenan <franck.kamenan@yahoo.com>
2020-08-22 12:12:41 +0200

    CharAt

---
+++ CharAtApp.java
@@ -0,0 +1,23 @@
+package be.intecbrussel.hoofdstuk6.charAt;
+
+public class CharAtApp
+{
+    public static void main(String[] args)
+    {
+        String monString = "C'est moi.";
+        String monObjet = new String("C'est un objet.");
+
+        System.out.println(monString);
+        System.out.println(monObjet);
+        System.out.println(monString.charAt(0));
+        System.out.println(monString.charAt(1));
+        System.out.println(monString.charAt(2));
+        System.out.println(monString.charAt(3));
+        System.out.println(monString.charAt(4));
+        System.out.println(monString.charAt(5));
+        System.out.println(monString.charAt(6));
+        System.out.println(monString.charAt(7));
+        System.out.println(monString.charAt(8));
+        System.out.println(monString.charAt(9));
+    }
+}
